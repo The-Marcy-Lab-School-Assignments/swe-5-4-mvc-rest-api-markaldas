95b294c6a2bec3b6ccef2ccd91db788ffdc8f6fb
markaldas <markaldas41@gmail.com>
2026-03-19 02:31:59 -0400

    finished draft

--- server/index.js
+++ server/index.js
@@ -25,28 +25,86 @@ app.use(express.json());
 
 
 // Increments and returns a unique id each time it is called.
-// let id = 1;
-// const getId = () => id++;
+let id = 1;
+const getId = () => id++;
 
 // Seed data — do not remove
-// const todos = [
-//   { id: getId(), task: 'Buy groceries', isDone: false },
-//   { id: getId(), task: 'Walk the dog', isDone: true },
-//   { id: getId(), task: 'Read a book', isDone: false },
-// ];
+const todos = [
+  { id: getId(), task: 'Buy groceries', isDone: false },
+  { id: getId(), task: 'Walk the dog', isDone: true },
+  { id: getId(), task: 'Read a book', isDone: false },
+];
 
 ////////////////////////
 // Endpoints
 ////////////////////////
-const todoControllers = require('../server/controllers/controllers.js')
+// const todoControllers = require('./controllers/controllers.js')
 
+const listTodos = (req, res) => {
+  res.send(todos);
+}
+
+const findTodo = (req, res) => {
+  const { id } = req.params;
+  const task = todos.find((task) => task.id === Number(id));
+  if (!task) {
+    return null;
+  }
+  if (!findTodo) {
+    res.status(404).send({ message: `no task with the id ${id} was found` });
+  }
+  res.send(task);
+}
+
+const createTodo = (req, res) => {
+  const { task } = req.body;
+  if (!task) {
+    res.status(400).send({ message: 'Please input task' });
+  }
+  const newTask = { id: getId(), task, isDone: false };
+  todos.push(newTask);
+  res.status(201).send(newTask);
+}
+
+const updateTodo = (req, res) => {
+  const { id } = req.params;
+  const { isDone } = req.body;
+  if (!id) {
+    res.status(404).send({ message: `no task with the id ${id} was found` })
+  }
+
+  const updateTask = todos.find((task) => task.id === Number(id));
+  if (!updateTask) {
+    res.status(404).send({ message: 'id not found' });
+  }
+  updateTask.isDone = isDone;
+  res.send(updateTask);
+}
+
+const deleteTodo = (req, res) => {
+  const { id } = req.params;
+  if (!id) {
+    res.status(404).send({ message: `no task with the id ${id} was found` })
+  }
+  const deleteTask = todos.findIndex((task) => task.id === Number(id));
+  if (deleteTask < 0) {
+    res.status(404).send({ message: 'Not found' });
+  }
+  todos.splice(deleteTask, 1);
+  res.sendStatus(204)
+}
 // TODO: GET /api/todos
 // Response: 200, array of all todos
-app.get('/api/todos', todoControllers.listTodos);
-app.get('/api/todos/:id', todoControllers.findTodo);
-app.post('/api/todos', todoControllers.createTodo);
-app.patch('/api/todos/:id', todoControllers.updateTodo);
-app.delete('/api/todos/:id', todoControllers.deleteTodo);
+
+const apiNotFound = (req, res) => {
+  res.status(404).send({ error: `Not found: ${req.originalUrl}` })
+}
+app.get('/api/todos', listTodos);
+app.get('/api/todos/:id', findTodo);
+app.post('/api/todos', createTodo);
+app.patch('/api/todos/:id', updateTodo);
+app.delete('/api/todos/:id', deleteTodo);
+app.use(apiNotFound);
 
 
 // TODO: GET /api/todos/:id
--- server/models/models.js
+++ server/models/models.js
@@ -1,45 +1,45 @@
-let id = 1;
-const getId = () => id++;
+// let id = 1;
+// const getId = () => id++;
 
-const todos = [
-    { id: getId(), task: 'Buy groceries', isDone: false },
-    { id: getId(), task: 'Walk the dog', isDone: true },
-    { id: getId(), task: 'Read a book', isDone: false },
-];
+// const todos = [
+//     { id: getId(), task: 'Buy groceries', isDone: false },
+//     { id: getId(), task: 'Walk the dog', isDone: true },
+//     { id: getId(), task: 'Read a book', isDone: false },
+// ];
 
-module.exports.list = () => {
-    return [...todos];
-};
+// module.exports.list = () => {
+//     return [...todos];
+// };
 
-module.exports.find = (id) => {
-    const task = todos.find((task) => task.id === id);
-    if (!task) {
-        return null;
-    }
-    return { ...task };
-};
+// module.exports.find = (id) => {
+//     const task = todos.find((task) => task.id === id);
+//     if (!task) {
+//         return null;
+//     }
+//     return { ...task };
+// };
 
-module.exports.create = (task) => {
-    const newTask = { id: getId(), task, isDone: false };
-    todos.push(newTask);
-    return { ...newTask };
-};
+// module.exports.create = (task) => {
+//     const newTask = { id: getId(), task, isDone: false };
+//     todos.push(newTask);
+//     return { ...newTask };
+// };
 
-module.exports.update = (id, isDone) => {
-    const updateTask = todos.find((task) => task.id === id);
-    if (!updateTask) {
-        return null;
-    }
-    updateTask.isDone = isDone;
-    return { ...updateTask };
+// module.exports.update = (id, isDone) => {
+//     const updateTask = todos.find((task) => task.id === id);
+//     if (!updateTask) {
+//         return null;
+//     }
+//     updateTask.isDone = isDone;
+//     return { ...updateTask };
 
-}
+// }
 
-module.exports.destroy = (id) => {
-    const deleteTask = todos.findIndex((task) => task.id === id);
-    if (deleteTask < 0) {
-        return null;
-    }
-    todos.splice(deleteTask, 1);
-    return true;
-}
+// module.exports.destroy = (id) => {
+//     const deleteTask = todos.findIndex((task) => task.id === id);
+//     if (deleteTask < 0) {
+//         return null;
+//     }
+//     todos.splice(deleteTask, 1);
+//     return true;
+// }
